261964747248d9d4c3ed40351fd143e5e50b4775
Jonathan Foster <jxf9099@rit.edu>
2018-05-23 02:17:58 -0400

    kiss

--- src/components/header.js
+++ src/components/header.js
@@ -9,21 +9,45 @@ const Header = () => (
       src={avatar}
       alt="illustration of me happily coding on my ThinkPad"
     />
-    <Name>Jonathan Foster</Name>
+    <Me>
+      <Name>Jonathan Foster</Name>
+      <Phrase>i'm still enjoying javascript</Phrase>
+    </Me>
     <Nav>
       <Link to="/resume/">Resume</Link>
     </Nav>
   </Container>
 );
 
+const Phrase = styled.div`
+  color: #939393;
+  margin-top: 0.5rem;
+`;
+
+const Me = styled.div`
+  margin: 0 1rem;
+`;
+
 const Name = styled.h1`
   font-weight: normal;
   color: #708090;
-  margin-left: 11rem;
   font-size: 2rem;
+  margin: 0;
+
+  span {
+    transition: opacity linear 400ms;
+    color: green;
+    opacity: 0;
+
+    &;hover {
+      opacity: 1;
+    }
+  }
 `;
 
 const Nav = styled.nav`
+  margin-left: 1rem;
+
   a {
     text-decoration: none;
     color: #6c7a89;
@@ -38,20 +62,14 @@ const Nav = styled.nav`
 const Container = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
-  background: #fff;
   padding: 1rem;
   height: 6rem;
-  border-bottom: 2px solid #eee;
 `;
 
 const Avatar = styled.img`
-  width: 10rem;
-  border-radius: 1rem;
+  max-height: 100%;
+  border-radius: 0.5rem;
   border: 2px solid #afbba8;
-  position: absolute;
-  top: 1rem;
-  left: 1rem;
 `;
 
 export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,17 @@ import styled from "styled-components";
 
 const IndexPage = () => (
   <Container>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus,
-    eligendi? Ea et molestias, pariatur molestiae consectetur nisi. Dolorem
-    quasi accusamus accusantium facere deleniti. Placeat repellat commodi
-    labore! Quas, nesciunt animi.
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus,
+      eligendi? Ea et molestias, pariatur molestiae consectetur nisi. Dolorem
+      quasi accusamus accusantium facere deleniti. Placeat repellat commodi
+      labore! Quas, nesciunt animi.
+    </p>
   </Container>
 );
 
 const Container = styled.div`
   padding: 1rem;
-  margin-left: 11rem;
   max-width: 5in;
 `;
 
