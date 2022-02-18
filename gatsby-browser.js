2a4f7ce29a3ae6968c246167ee24667e2cb060ae
Jonathan Foster <fostertheweb@gmail.com>
2022-02-18 00:50:21 -0500

    tailwind, prettier, page stub

---
+++ gatsby-browser.js
@@ -0,0 +1,4 @@
+import "@fontsource/montagu-slab";
+import "@fontsource/outfit";
+
+import "./src/styles/global.css";
---
+++ src/pages/wedding/jake.js
@@ -0,0 +1,36 @@
+import React from "react";
+import reyna from "../../images/reyna.png";
+
+export default function JakePage() {
+  return (
+    <div className="flex w-full min-h-screen justify-center items-center bg-orange-100">
+      <div className="flex flex-col w-1/3 gap-6">
+        <div className="flex items-end justify-between">
+          <h1 className="ftw-font-outfit font-bold text-6xl text-red-900">
+            Jake
+          </h1>
+          <img src={reyna} alt="reyna" className="w-48" />
+        </div>
+        <p className="text-red-900">
+          The unrelenting orgasms from his cumtree pounding my split peach made
+          me come so hard, I began sweating like a pregnant nun. After having my
+          herring hole slammed, he then proceeded to pound my brown eye. The
+          raiding makes me flood my sex wee all over his bald avenger. By now,
+          my oyster ditch was foaming like a rabid dog. My ground zero grotto
+          was trembling like Micheal J. Fox licking a car battery.
+        </p>
+        <h2 className="text-red-900 font-bold ftw-font-outfit tracking-wide text-2xl">
+          Will you be my groomsman?
+        </h2>
+        <div className="flex items-center gap-4">
+          <button className="p-4 rounded font-medium text-lg flex-grow bg-orange-200 text-red-900">
+            No
+          </button>
+          <button className="p-4 rounded flex-grow font-medium text-lg bg-orange-300 text-red-900">
+            Yes
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
