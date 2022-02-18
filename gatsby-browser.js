86f2170327213eb86c59588601c933e18f5443c5
Jonathan Foster <fostertheweb@gmail.com>
2022-02-18 02:13:51 -0500

    mdx is sus

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,3 @@
-import "@fontsource/montagu-slab";
 import "@fontsource/outfit";
 
 import "./src/styles/global.css";
--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,6 @@ module.exports = {
         icon: "src/images/icon.png",
       },
     },
-    "gatsby-plugin-mdx",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
