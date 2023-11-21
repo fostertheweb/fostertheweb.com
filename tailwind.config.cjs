0a3c3f2c6314d320909eaf1de95fd17ce98eefd0
Jonathan Foster <fostertheweb@gmail.com>
2023-11-21 15:52:46 -0500

    fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				serif: ["Calistoga", "Georgia", "serif"]
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ["IBM Plex Serif", "Georgia", "serif"],
+        sans: ["IBM Plex Sans", "sans-serif"],
+        mono: ["IBM Plex Mono", "Consolas", "Menlo", "monospace"],
+      },
+    },
+  },
+  plugins: [],
+};
