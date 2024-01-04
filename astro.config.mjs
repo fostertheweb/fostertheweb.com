8e4efbf08c9db97e03904bc2c2ed2f545da99b59
Jonathan Foster <fostertheweb@gmail.com>
2024-01-04 12:03:21 -0500

    automatic last modified, smaller about

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import { remarkModifiedTime } from "./plugins/remark-modified-time.mjs";
 
 import netlify from "@astrojs/netlify/functions";
 
@@ -8,11 +9,12 @@ import netlify from "@astrojs/netlify/functions";
 export default defineConfig({
   output: "hybrid",
   markdown: {
+    remarkPlugins: [remarkModifiedTime],
     shikiConfig: {
       theme: "dark-plus",
-      wrap: true
-    }
+      wrap: true,
+    },
   },
   integrations: [tailwind(), mdx()],
-  adapter: netlify()
+  adapter: netlify(),
 });
