82452311c7da19f366a77e03515d9d11ad697fce
Jonathan Foster <fostertheweb@gmail.com>
2024-01-26 02:30:58 -0500

    gruvbox

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,21 @@
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import rehypePrettyCode from "rehype-pretty-code";
 import { remarkModifiedTime } from "./plugins/remark-modified-time.mjs";
 
 import netlify from "@astrojs/netlify";
+
+const gruvboxDark = JSON.parse(fs.readFileSync("gruvbox-dark.json", "utf8"));
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   markdown: {
+    rehypePlugins: [rehypePrettyCode],
     remarkPlugins: [remarkModifiedTime],
     shikiConfig: {
-      experimentalThemes: {
-        light: "github-light",
-        dark: "github-dark",
-      },
+      theme: gruvboxDark,
       wrap: true,
     },
   },
